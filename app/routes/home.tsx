923b32a9723a16f4c06aaa559e7ebb3f7a5998ab
SHENG-WEI TSAI <henry88002605@gmail.com>
2025-10-26 09:53:26 +1000

    authentication & loginout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import ResumeCard from "../components/ResumeCard";
 import {resumes} from  "../../constants";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,8 +14,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {isLoading, auth} = usePuterStore();
+  const navigate  = useNavigate();
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated]);
+
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
+
     <section className="main-section">
       <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
